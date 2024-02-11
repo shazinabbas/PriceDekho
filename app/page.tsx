1e4a0e2d4a372714dfc19c43f93255f268c0be45
Shazinabbas <shazinabbas_m@srmap.edu.in>
2024-02-11 23:08:04 +0530

    Searchbar updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Searchbar from '@/components/Searchbar'
 import Image from 'next/image'
 import React from 'react'
 
@@ -27,7 +28,7 @@ const Home = () => {
             Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
             </p>
 
-            Searchbar
+            <Searchbar/>
           </div>
 
           HeroCarousel
